5e88aea7c43aec3a9d48598504b48a951497fddf
Drew Barkman <djbarkman25@gmail.com>
2026-02-23 14:50:14 -0600

    please...

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.drewbarkman.com',
+  site: 'https://drewbarkman.com',
 })
